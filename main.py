98a8ec8b813f1782d8e026379771140d1fff98f2
Manas <iamscientistmanas@gmail.com>
2024-02-16 16:24:15 +0530

    add script

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from colorama import Fore, Style
+import sys
+import subprocess
+
+
 
 logo = """
-
-
+░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░     
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░     
+                                                                            
 """
 
+print(logo)
+
+
+# Logic starts
+
+print(f'{Style.BRIGHT}The following tools are available: {Style.RESET_ALL}')
+print()
+
+print(f"{Style.BRIGHT}{Fore.YELLOW}1){Fore.RESET}Your browser history safety{Style.RESET_ALL}")
+print(f"{Style.BRIGHT}{Fore.YELLOW}2){Fore.RESET}Is your email exposed ? {Style.RESET_ALL}")
+print(f"{Style.BRIGHT}{Fore.YELLOW}3){Fore.RESET}Perform Dark Web Crawling [Fore Professionals]{Style.RESET_ALL}")
+
+
+print(f"{Style.BRIGHT}{Fore.BLACK} Tool want to use? {Fore.RESET} {Style.RESET_ALL}")
+
+user_choice = int(input())
+
+if  user_choice == 1 :
+    subprocess.run(["python3", 'UserSafety.py'])
+elif user_choice == 2 :
+    subprocess.run(["python3", "data_breach_checker.py"])
+elif user_choice == 3 :
+    pass
+else :
+    print("Exiting the program...")
+    sys.exit()
